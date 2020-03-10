92185240465abe0eb9a9cdc0a70c51d1f06fca42
ramzannoon <57062791+ramzannoon@users.noreply.github.com>
2020-03-10 15:12:18 +0500

    some changes added

--- models/sale_order_status.py
+++ models/sale_order_status.py
@@ -3,8 +3,8 @@ from odoo import models, fields, api, _
 class Saleorderstatus(models.Model):
     _inherit = 'sale.order'
 
-    sale_order_status = fields.Selection([('in-process', 'In-process'),
+    sale_order_status = fields.Selection([('in-process', 'In Process'),
                                     ('shipped', 'Shipped'),
                                     ('pending', 'Pending'),
                                     ('cancelled', 'Cancelled'),
-                                    ], default='in-process', string='Status')
+                                    ], default='in-process', string='Order Status')
